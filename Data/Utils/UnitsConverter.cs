3cce0836dd85a06f2ffe0575c119b56bf6d8eb17
KrzesimirPrzybylski <krzem.1999@gmail.com>
2021-12-21 23:30:57 +0100

    Przywrócenie działającej wersjii

---
+++ Data/Utils/UnitsConverter.cs
@@ -0,0 +1,19 @@
+﻿
+namespace LocalDatabase_Client.Data.Utils
+{
+    public static class UnitsConverter
+    {
+
+        public static double ConvertBytesToGigabytes(long bytes)
+        {
+            double resultGigabytes = bytes / 1024.0 / 1024.0 / 1024.0;
+            return resultGigabytes;
+        }
+
+        public static long ConvertGigabytesToBytes(long gigabytes)
+        {
+            long resultBytes = gigabytes * 1024 * 1024 * 1024;
+            return resultBytes;
+        }
+    }
+}
