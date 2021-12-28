e81972d8f2a073036efd6babc9aff4c5a38ac4a5
alicja.mruk <alicja.mruk99@gmail.com>
2021-12-28 01:03:01 +0100

    Formatting

--- Security/DecryptionFile.cs
+++ Security/DecryptionFile.cs
@@ -1,56 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.IO;
 using System.Security.Cryptography;
-
 
 namespace LocalDatabase_Client.Security
 {
     public class DecryptionFile
     {
-
-        public static void Decrypt(string inFile, string user_key)
+        public static void Decrypt(string inFile, string userKey)
         {
-            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(user_key);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(userKey);
             byte[] salt = new byte[32];
+            int iterationsNumber = 50000;
 
             FileStream fsCrypt = new FileStream(inFile, FileMode.Open);
             fsCrypt.Read(salt, 0, salt.Length);
 
             RijndaelManaged AES = new RijndaelManaged();
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
-            AES.Key = key.GetBytes(32); //zozmiar klucza 
-            AES.IV = key.GetBytes(16); //rozmiar bloka
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, iterationsNumber);
+            AES.Key = key.GetBytes(32); //key size
+            AES.IV = key.GetBytes(16); //block size
             AES.Padding = PaddingMode.PKCS7;
 
-            //Tryb szyfrowania AES: ECB, CBC, CFB, OFB, CTR
+            //CipherMode AES: ECB, CBC, CFB, OFB, CTR
             AES.Mode = CipherMode.CFB;
 
-
             string dirName = Path.GetDirectoryName(inFile);
-            string fName = Path.GetFileNameWithoutExtension(inFile);
-            string fExtension = Path.GetExtension(inFile);
+            string fileName = Path.GetFileNameWithoutExtension(inFile);
+            string fileExtension = Path.GetExtension(inFile);
             
             CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
-            FileStream fsOut = new FileStream(dirName + "\\" + fName, FileMode.Create);
+            FileStream fsOut = new FileStream(dirName + "\\" + fileName, FileMode.Create);
 
             int read;
             byte[] buffer = new byte[1048576];
-
 
             while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
                 fsOut.Write(buffer, 0, read);
 
             cs.Close();
 
-            File.Delete(@dirName + "\\" + fName + fExtension);
+            File.Delete(@dirName + "\\" + fileName + fileExtension);
             fsOut.Close();
             fsCrypt.Close();
-        
         }
-
     }
 }
