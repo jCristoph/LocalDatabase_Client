067d3f6be8be0129d64a5a139a60fcbfa30eadd8
alicja.mruk <alicja.mruk99@gmail.com>
2021-12-28 01:17:37 +0100

    Ref code

--- Security/KeyGenerator.cs
+++ Security/KeyGenerator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LocalDatabase_Client.Security
 {
     public static class KeyGenerator
@@ -11,40 +6,53 @@ namespace LocalDatabase_Client.Security
         private static System.Random r = new System.Random();
         public static string Generate()
         {
-            int key_len = 32; //klucz o długości 256 bitów
+            int keyLength = 32; //klucz o długości 256 bitów
 
             //Alfabet przygotowany do stworzenia klucza:
-            char[] b_letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'X', 'Z' };
-            char[] s_letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z' };
+            char[] uppercaseLetters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'X', 'Z' };
+            char[] lowercaseLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z' };
             char[] numbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             char[] symbols = new char[] { '!', '@', '#', '$', '%', '&', '*', '=', ']', '[', '/', '+', '~' };
 
+            string shuffleResult = ShuffleKey(keyLength, uppercaseLetters, lowercaseLetters, numbers, symbols);
+            return shuffleResult;
+        }
 
+        private static string ShuffleKey(int keyLength, char[] uppercaseLetters, char[] lowercaseLetters, char[] numbers, char[] symbols )
+        {
             string key = string.Empty;
 
-            for (int i = 0; i < key_len; ++i)
+            for (int i = 0; i < keyLength; ++i)
             {
                 int random = r.Next(4);
-                if (random == 0)
+                switch (random)
                 {
-                    int location = r.Next(26);
-                    key += b_letters[location];
+                    case 0:
+                        {
+                            int location = r.Next(26);
+                            key += uppercaseLetters[location];
+                            break;
+                        }
+                    case 1:
+                        {
+                            int location = r.Next(10);
+                            key += numbers[location];
+                            break;
+                        }
+                    case 2:
+                        {
+                            int location = r.Next(26);
+                            key += lowercaseLetters[location];
+                            break;
+                        }
+                    default:
+                        {
+                            int location = r.Next(13);
+                            key += symbols[location];
+                            break;
+                        }
                 }
-                else if (random == 1)
-                {
-                    int location = r.Next(10);
-                    key += numbers[location];
-                }
-                else if (random == 2)
-                {
-                    int location = r.Next(26);
-                    key += s_letters[location];
-                }
-                else
-                {
-                    int location = r.Next(13);
-                    key += symbols[location];
-                }
+
             }
             return key;
         }
